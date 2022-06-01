2267341c172252d6df30bfe53eb411e142baad0f
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-06-01 20:37:09 +0100

    first part home page

--- src/app/components/lower-banner/lower-banner.component.ts
+++ src/app/components/lower-banner/lower-banner.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lower-banner',
   templateUrl: './lower-banner.component.html',
   styleUrls: ['./lower-banner.component.scss']
 })
-export class LowerBannerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class LowerBannerComponent { }
