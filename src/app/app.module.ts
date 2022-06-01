47302e0c4b2676efe27c289f1fe8a4577d8bedc9
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-06-01 10:31:47 +0100

    added nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppRoutingModule.components
+    AppRoutingModule.components,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
