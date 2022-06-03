a710f34050358d7fb6a7e0a23ffe733c7ea28254
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-06-03 15:01:20 +0100

    connected routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit, OnDestroy {
+
+  contactPage = false;
+  private contactUrl = '/contact';
+  protected sub!: Subscription;
+
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+    this.sub = this.router.events
+    .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+    .subscribe(event => this.contactPage = event.url === this.contactUrl)
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+}
