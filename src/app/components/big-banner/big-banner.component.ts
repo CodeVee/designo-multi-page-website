7815b45298a94e75edf8f5239e18ca2debe16f23
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-06-03 23:31:17 +0100

    made tablet responsive

--- src/app/components/big-banner/big-banner.component.ts
+++ src/app/components/big-banner/big-banner.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-banner',
   templateUrl: './big-banner.component.html',
   styleUrls: ['./big-banner.component.scss']
 })
-export class BigBannerComponent { }
+export class BigBannerComponent {
+  @Input() page = ''
+ }
