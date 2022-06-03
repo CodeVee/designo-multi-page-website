9cadc5c687d7e3eb93e6ef65ecec6138bfe89344
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-06-03 12:45:33 +0100

    added navigation

--- src/app/components/lower-banner/lower-banner.component.ts
+++ src/app/components/lower-banner/lower-banner.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lower-banner',
   templateUrl: './lower-banner.component.html',
   styleUrls: ['./lower-banner.component.scss']
 })
-export class LowerBannerComponent { }
+export class LowerBannerComponent {
+
+  constructor(private router: Router) { }
+
+  contact(): void {
+    this.router.navigate(['/contact']);
+  }
+}
