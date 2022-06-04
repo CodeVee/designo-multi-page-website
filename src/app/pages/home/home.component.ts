f98c372d56c726a640c0ea94250ac64d9b1d84bc
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-06-04 14:24:59 +0100

    added titles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { DesignType } from 'src/app/models/design.model';
 
 @Component({
@@ -6,6 +7,12 @@ import { DesignType } from 'src/app/models/design.model';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   designTypes = [DesignType.WebDesignLarge, DesignType.AppDesign, DesignType.GraphicDesign]
+
+  constructor(private title: Title) { }
+
+  ngOnInit(): void {
+      this.title.setTitle('Designo: Home');
+  }
 }
